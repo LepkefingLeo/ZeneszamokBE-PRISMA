bcc5fa03cb91ecb6ad2e37b415124d4e290e6f56
Petrik Diak <diak@petrik.hu>
2025-11-24 13:09:21 +0100

    Songs

---
+++ src/songs/songs.controller.ts
@@ -0,0 +1,43 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { SongsService } from './songs.service';
+import { CreateSongDto } from './dto/create-song.dto';
+import { UpdateSongDto } from './dto/update-song.dto';
+import { title } from 'process';
+
+@Controller('songs')
+export class SongsController {
+  constructor(private readonly songsService: SongsService) {}
+
+  @Post()
+  create(@Body() createSongDto: CreateSongDto) {
+    return this.songsService.create(createSongDto);
+  }
+
+  // URl:/songs?start=12&count=10
+  @Get()
+  findAll(
+    @Query('start') start: string = '0',
+    @Query('count') count: string = '10') {
+    return this.songsService.findAll(+start, +count);
+  }
+
+  @Get('search')
+  search(@Query('title') title: string) {
+    return this.songsService.search(title);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.songsService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateSongDto: UpdateSongDto) {
+    return this.songsService.update(+id, updateSongDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.songsService.remove(+id);
+  }
+}
